7687e3e13fa673058138f17103ffb47f2f9e06b8
BigTrevHD <34038408+BigTrevHD@users.noreply.github.com>
2019-04-04 17:58:26 +0100

    Start to Shelter Queue
    Very imcomplete, need to rethink whole process

---
+++ Assets/shelter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shelter : MonoBehaviour {
+    public digitalClock clock;
+    public gameMaster gm;
+
+    private int startTime;
+
+    private int queueTime;
+    private int timeSinceWait;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void Queue()
+    {
+        startTime = clock.hour;
+        InvokeRepeating("ReduceQueue", 0f, gm.timeSpeed);
+    }
+
+    void ReduceQueue()
+    {
+
+    }
+}
