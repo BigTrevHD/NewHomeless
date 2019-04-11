ea3c97f6b4bda4c4a5ea7dab6ff86cfb9c05d52a
BigTrevHD <34038408+BigTrevHD@users.noreply.github.com>
2019-04-11 14:52:36 +0100

    cheeky push

---
+++ Assets/rummage.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rummage : MonoBehaviour
+{
+    public int chance;
+    public float amount;
+    private bool showBin;
+    public GameObject binInv;
+    public gameMaster gm;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (showBin)
+        {
+            binInv.SetActive(true);
+
+        }
+        else
+        {
+            binInv.SetActive(false);
+        }
+            
+
+        }
+        void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Bin"))
+            {
+            
+            if (Input.GetKeyDown(KeyCode.E))
+                 {
+                amount = Random.Range(0f, 2f);
+                chance = Random.Range(61, 100);
+                if (chance > 60)
+                    {
+                        gm.cash += amount;
+                        amount = 0;
+
+                }
+            }
+        }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bin"))
+        {
+            showBin = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Bin"))
+        {
+            showBin = false;
+        }
+    }
+}
