f38e28f79eb0603f24d042aa4f0b55c372c67cf9
TOPHATs64GAMEs <34027835+TOPHATs64GAMEs@users.noreply.github.com>
2019-05-01 17:55:21 +0100

    yup

---
+++ Assets/Scripts/carmovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carmovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * -Time.deltaTime, 0f, 0f);
+       // transform.Rotate(0, 90, 0);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.tag == "Car")
+        {
+            transform.Rotate(0, 90, 0);
+        }
+    }
+
+
+}
+
+
